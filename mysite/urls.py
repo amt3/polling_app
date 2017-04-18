81edd584ae3dd30b590c4a44f07bffd2753248de
amt3 <amtpagrut@gmail.com>
2017-04-18 20:47:57 +0530

    urls updated

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^polls/', include('polls.urls')),
